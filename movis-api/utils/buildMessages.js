214b5717f8ce6517954f3a03f57e17f03f6ee828
Jose Garcia <jagarcia7655@gmail.com>
2020-11-05 08:53:57 -0500

    Ultimos cambios antes de deploy

--- movis-api/utils/buildMessages.js
+++
@@ -1,9 +0,0 @@
-const buildMessages = (entity, action) => {
-  if(action === 'list') {
-    return `${entity}s ${action}ed`;
-  }
-
-  return `${entity} ${action}d`
-}
-
-module.exports = buildMessages
--- movis-api/utils/cacheResponse.js
+++
@@ -1,9 +0,0 @@
-const { config } = require('../config/index');
-
-function cacheResponse(res, seconds) {
-  if(!config.dev) {
-    res.set("Cache-Control", `public, max-age=${seconds}`);
-  }
-}
-
-module.exports = cacheResponse;
